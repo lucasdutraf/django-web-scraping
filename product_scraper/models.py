f9ce153f84e1cfa1c0ff8fd17eb56bab28fb8b4b
Lucas Dutra <ldutra98@gmail.com>
2019-05-26 18:47:12 -0300

    Change price attribute type from decimal to charfield

--- product_scraper/models.py
+++ product_scraper/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product(models.Model):
     id = models.IntegerField(primary_key=True, blank=False)
     name = models.CharField(max_length=100, blank=False, null=False)
-    price = models.DecimalField(max_digits=5, blank=False, null=False, decimal_places=3)
+    price = models.CharField(max_length=50, blank=False, null=False)
     image_link = models.URLField(max_length=150)
 
     def __str__(self):
