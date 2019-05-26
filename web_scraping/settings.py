96b61b7d2b7d7a3e1b047f19774cf22ac2053d9d
Lucas Dutra <ldutra98@gmail.com>
2019-05-26 18:47:39 -0300

    Add rest framework to the installed apps

--- web_scraping/settings.py
+++ web_scraping/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'product_scraper.apps.ProductScraperConfig'
+    'product_scraper.apps.ProductScraperConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
