41a43f440e769287b8d7fca5efb1711d927e95db
Lucas Dutra <ldutra98@gmail.com>
2019-05-26 18:49:42 -0300

    Create get all products view and get unique product view

--- product_scraper/views.py
+++ product_scraper/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .db_helper import populate_product_db
+from .serializers import ProductSerializer
+from .models import Product
 
-# Create your views here.
+class ProductsList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductId(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
